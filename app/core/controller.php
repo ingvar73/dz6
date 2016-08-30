e2ebf721c3696d10d4ef068b5c105d24be51c2ec
ingvar73 <ingvar73@gmail.com>
2016-08-31 00:20:36 +0300

    а

---
+++ app/core/controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin-pc
+ * Date: 30.08.2016
+ * Time: 15:54
+ */
+
+class Controller
+{
+    public $model;
+    public $view;
+
+    function __construct(){
+        $this->view = new View();
+    }
+
+    function action_index(){
+        $this -> view -> generate('myview', 'index.php');
+    }
+
+//    function action_default(){
+//        echo "Action Default";
+//    }
+}
