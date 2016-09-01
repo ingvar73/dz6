c3f65e3f666fd576e06ef2ce287c6e5797e8e8dc
ingvar73 <ingvar73@gmail.com>
2016-09-01 19:57:45 +0300

    MVC

--- components/Db.php
+++ components/Db.php
@@ -6,11 +6,26 @@
  * Date: 29.08.2016
  * Time: 3:12
  */
+
+//$table_name = "gbook";
+//$host = "localhost";
+//$user = "mysql";
+//$pass = "";
+//$mysql = @new mysqli($host, $user, $pass, $table_name);
+//if(mysqli_connect_errno()){
+//    die(mysqli_connect_error());
+//}
+//$sql = "SET NAMES 'UTF8'";
+//$mysql->query($sql);
+$dir_name = str_replace('\\', '/', dirname(__FILE__));
+define ('ROOT', $dir_name);
+
 class Db
 {
     public static function getConnection(){
         $paramsPath = ROOT.'/config/db_params.php';
-        $params = include ($paramsPath);
+		echo $paramsPath;
+        $params = include($paramsPath);
 
         $dsn = "mysql:host={$params['host']}; dbname={$params['dbname']}";
         $db = new PDO($dsn, $params['user'], $params['password']);
